68b3d498f4dd893c1619aef04e798850773f28a3
RezkyKam50 <rezkykam34@gmail.com>
2025-10-16 14:21:48 +0700

    update

--- src/utils/Constellation/Loader.py
+++ src/utils/Constellation/Loader.py
@@ -2,7 +2,9 @@ import cudf
 
 
 def LoadDataset():
+    # for faster loading, use GPU DF
     plot_df_3d = cudf.read_parquet("./datasets/umapped3D.parquet", engine='pyarrow')
     plot_df_2d = cudf.read_parquet("./datasets/umapped2D.parquet", engine='pyarrow')
 
-    return plot_df_3d, plot_df_2d
+    # then move to CPU
+    return plot_df_3d.to_pandas(), plot_df_2d.to_pandas()
