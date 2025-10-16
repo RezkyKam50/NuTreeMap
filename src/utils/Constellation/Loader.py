339849983a91666794a6399a523b294c1186f72e
RezkyKam50 <rezkykam34@gmail.com>
2025-10-16 14:18:55 +0700

    RAPIDS

--- src/utils/Constellation/Loader.py
+++ src/utils/Constellation/Loader.py
@@ -1,8 +1,8 @@
-import pandas as pd
+import cudf
 
 
 def LoadDataset():
-    plot_df_3d = pd.read_parquet("./datasets/umapped3D.parquet", engine='pyarrow')
-    plot_df_2d = pd.read_parquet("./datasets/umapped2D.parquet", engine='pyarrow')
+    plot_df_3d = cudf.read_parquet("./datasets/umapped3D.parquet", engine='pyarrow')
+    plot_df_2d = cudf.read_parquet("./datasets/umapped2D.parquet", engine='pyarrow')
 
     return plot_df_3d, plot_df_2d
